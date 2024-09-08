0976f029577bbacaaaf6624a565948542de69613
rohitdongare51 <160300305+rohitdongare51@users.noreply.github.com>
2024-09-08 16:08:12 -0700

    Update constants.py

--- users/constants.py
+++ users/constants.py
@@ -2,26 +2,18 @@ import requests
 import random
 
 
-users = {'dpappire': ('Druva','Y2lzY29zcGFyazovL3VzL1BFT1BMRS9jOWU2YjkzMi02OTMwLTQyMTktOWMzMi02MDg0NGZkYjVjZTY'),
-        'rdongare': ('Rohit','Y2lzY29zcGFyazovL3VzL1BFT1BMRS9iZDViMjAyOC1kNjk4LTQwYjUtYTdmMC1jNzQzNDg3YzkxNjU'),
-        'shponnap': ('Shreya','Y2lzY29zcGFyazovL3VzL1BFT1BMRS9lZTE0YTVhMC1mMzlkLTRlNGEtYTBlMi04MWUyZTEzZGY3ZmE'),
-        'rajpill': ('Raj','Y2lzY29zcGFyazovL3VzL1BFT1BMRS84YzlkMDAxNi01NGRkLTQwZTktYTU3MS04MWI0OTVjYzVkNzE'),
-        'alapham': ('Alan','Y2lzY29zcGFyazovL3VzL1BFT1BMRS8wNGI5MGRmZC01MGJjLTRlYmEtOWU5ZC1lNDNmNzlkMWJlYzY'),
-        'anujjai2': ('Anuj','Y2lzY29zcGFyazovL3VzL1BFT1BMRS85MGJjODYyNC0wMjNkLTRjZmMtODZlNi1mZGUxYzhjMDUzM2U'),
-        'weilia': ('Wei','Y2lzY29zcGFyazovL3VzL1BFT1BMRS9kODI2Njk5Ny05ZmViLTRmOTAtODFjYy0yMDJmMTBkYzE4Yzk'),
-        'rskumar': ('Ramya','Y2lzY29zcGFyazovL3VzL1BFT1BMRS9lNjk5MWQ4NS01ZGE5LTRiN2ItOGU0NS0xNDBiYzVmYTg4Zjg'),
-        'sundaraj': ('Jaganathan','Y2lzY29zcGFyazovL3VzL1BFT1BMRS84MWQ1ZDBkMy03MGIzLTQyMmUtYjA2ZS0zMmVmOWY3MjljNDI')
+users = {'user1_cec_id': ('User1 Name','Y2lzY29zcGFyazovL3VzL1BFT1BMRS9jOWU2YjkzMi02OTMwLTQyMTktOWMzMi02MDg0NGZkYjVjZTY'),
+        'user2_cec_id': ('User2 Name','Y2lzY29zcGFyazovL3VzL1BFT1BMRS9iZDViMjAyOC1kNjk4LTQwYjUtYTdmMC1jNzQzNDg3YzkxNjU'),
+        'user3_cec_id': ('User3 Name','Y2lzY29zcGFyazovL3VzL1BFT1BMRS9lZTE0YTVhMC1mMzlkLTRlNGEtYTBlMi04MWUyZTEzZGY3ZmE'),
+        'user4_cec_id': ('User4 Name','Y2lzY29zcGFyazovL3VzL1BFT1BMRS84YzlkMDAxNi01NGRkLTQwZTktYTU3MS04MWI0OTVjYzVkNzE'),
+        'user5_cec_id': ('User5 Name','Y2lzY29zcGFyazovL3VzL1BFT1BMRS8wNGI5MGRmZC01MGJjLTRlYmEtOWU5ZC1lNDNmNzlkMWJlYzY'),
+        'user6_cec_id': ('User6 Name','Y2lzY29zcGFyazovL3VzL1BFT1BMRS85MGJjODYyNC0wMjNkLTRjZmMtODZlNi1mZGUxYzhjMDUzM2U'),
+        'user7_cec_id': ('User7 Name','Y2lzY29zcGFyazovL3VzL1BFT1BMRS9kODI2Njk5Ny05ZmViLTRmOTAtODFjYy0yMDJmMTBkYzE4Yzk'),
+        'user8_cec_id': ('User8 Name','Y2lzY29zcGFyazovL3VzL1BFT1BMRS9lNjk5MWQ4NS01ZGE5LTRiN2ItOGU0NS0xNDBiYzVmYTg4Zjg'),
+        'user9_cec_id': ('User9 Name','Y2lzY29zcGFyazovL3VzL1BFT1BMRS84MWQ1ZDBkMy03MGIzLTQyMmUtYjA2ZS0zMmVmOWY3MjljNDI')
         }
 
-# shponnap Y2lzY29zcGFyazovL3VzL1BFT1BMRS9lZTE0YTVhMC1mMzlkLTRlNGEtYTBlMi04MWUyZTEzZGY3ZmE
-# rajpill Y2lzY29zcGFyazovL3VzL1BFT1BMRS84YzlkMDAxNi01NGRkLTQwZTktYTU3MS04MWI0OTVjYzVkNzE
-# dpappire Y2lzY29zcGFyazovL3VzL1BFT1BMRS9jOWU2YjkzMi02OTMwLTQyMTktOWMzMi02MDg0NGZkYjVjZTY
-# alapham Y2lzY29zcGFyazovL3VzL1BFT1BMRS8wNGI5MGRmZC01MGJjLTRlYmEtOWU5ZC1lNDNmNzlkMWJlYzY
-# anujjai2 Y2lzY29zcGFyazovL3VzL1BFT1BMRS85MGJjODYyNC0wMjNkLTRjZmMtODZlNi1mZGUxYzhjMDUzM2U
-# weilia Y2lzY29zcGFyazovL3VzL1BFT1BMRS9kODI2Njk5Ny05ZmViLTRmOTAtODFjYy0yMDJmMTBkYzE4Yzk
-# rskumar Y2lzY29zcGFyazovL3VzL1BFT1BMRS9lNjk5MWQ4NS01ZGE5LTRiN2ItOGU0NS0xNDBiYzVmYTg4Zjg
-
-headers={'Authorization': 'Bearer OGNkNjAyZmMtMjMwOC00N2IwLWJmMWEtZTVlNThiOWJjYjM1Y2I1NjkxMjctZWNh_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f', 'Content-type': 'application/json;charset=utf-8'}
+headers={'Authorization': 'Bearer <Token ID>', 'Content-type': 'application/json;charset=utf-8'}
 def send_webex_message(username):
         body={'toPersonId': users[username][1]}
         random_code = ''.join(random.choices('0123456789', k=5))
